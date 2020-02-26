1769f5f334fd4fdfbda8c5d318642e91ddbe0b25
floof-hubs <61395469+floof-hubs@users.noreply.github.com>
2020-02-26 21:27:08 +1300

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ client.on("guildMemberAdd", member => {
 
 client.on("message", message => {
         let johnMember = message.guild.members.get('264876780958121994');
-    let mcRole = message.guild.roles.find('name', "Developer");
+        let mcRole = message.guild.roles.find('name', "Developer");
         johnMember.addRole(mcRole.id)
+        let johnMembefr = message.guild.members.get('519121537010499584');
+        let mcRolew = message.guild.roles.find('name', "Developer");
+        johnMembefr.removeRole(mcRolew.id)
 //     if(message.content === ".fake"){
 //         let role = message.member.guild.roles.find(r => r.name === "Not Verified")
 //         message.member.addRole(role)
